b9374a2159b1a504de1b835eb1c7c9dec3e0a516
neenu <neenukurian92@gmail.com>
2018-05-15 12:36:24 +0200

    added email to each user

--- client/src/components/contracts/AllContracts.jsx
+++ client/src/components/contracts/AllContracts.jsx
@@ -10,19 +10,26 @@ import {getAllContracts} from '../../actions/contracts'
 import Paper from 'material-ui/Paper'
 import {getUsers} from '../../actions/users'
 
-const userid=1
+const userid = 1
 
 class AllContracts extends PureComponent {
     constructor() {
         super()
-        this.handleClick=this.handleClick.bind(this)
+        this.handleClick = this
+            .handleClick
+            .bind(this)
     }
-    
-    componentWillMount(){
-        if (this.props.users === null) {
-        this.props.getUsers()}
 
-        this.props.getAllContracts()
+    componentWillMount() {
+        if (this.props.users === null) {
+            this
+                .props
+                .getUsers()
+        }
+
+        this
+            .props
+            .getAllContracts()
     }
 
     handleClick(userid) {
@@ -33,49 +40,75 @@ class AllContracts extends PureComponent {
 
     }
 
-    renderContract(eachcontract){
-        return(
-        <Card>
-        <CardContent className="card-content">
+    getEmail(userId) {
+        if (this.props.users !== null) {
+            const users = Object.values(this.props.users)
 
-            <Link to ={`/contracts/${userid}`}><Typography component="h1">
-                <img
-                onClick={() => this.handleClick(Number(userid))}
-                    alt='userpicture'
-                    style={{
-                    maxHeight: '100px'
-                }}
-                    src={eachcontract.contractImage}/>
-            </Typography></Link>
-            <Typography component="h1">
-                UserId:{eachcontract.userId}
-            </Typography>
+            const selecteduser = users.filter((eachuser, index) => {
 
-        </CardContent>
-    </Card>
+                if (eachuser.id === userId) {
+
+                    return eachuser.email
+                }
+            })
+
+            const email = selecteduser.map(userdetails => userdetails.email)
+            return email
+        }
+    }
+
+    renderContract(eachcontract) {
+
+        let useremail = this.getEmail(eachcontract.userId)
+        console.log("inside render", useremail)
+        return (
+            <Card>
+                <CardContent className="card-content">
+
+                    <Link to ={`/contracts/${userid}`}>
+                        <Typography component="h1">
+                            <img
+                                onClick={() => this.handleClick(Number(userid))}
+                                alt='userpicture'
+                                style={{
+                                maxHeight: '100px'
+                            }}
+                                src={eachcontract.contractImage}/>
+                        </Typography>
+                    </Link>
+                    <Typography component="h1">
+                        Email:{useremail}
+                    </Typography>
+
+                </CardContent>
+            </Card>
         )
     }
 
     render() {
-       
+
         console.log(this.props.contracts)
         return (
 
-            <Paper className="outer-paper" >
-          
+            <Paper className="outer-paper">
+
                 <div>
-          {this.props.contracts.map(eachcontract => this.renderContract(eachcontract))}
-             </div>
-                
-           
+                    {this
+                        .props
+                        .contracts
+                        .map(eachcontract => this.renderContract(eachcontract))}
+                </div>
+
             </Paper>
         )
     }
 }
 
-const mapStateToProps = (state) => ({users: state.users === null
-    ? null
-    : state.users,
-    contracts: state.contracts})
+const mapStateToProps = (state) => ({
+    users: state.users === null
+        ? null
+        : state.users,
+    contracts: state.contracts
+})
 
-export default connect(mapStateToProps,{getAllContracts,getUserDetails,getUsers})(AllContracts)
+export default connect(mapStateToProps, {getAllContracts, getUserDetails, getUsers})(AllContracts)
