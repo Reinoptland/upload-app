b82b0db07ed289bec49d9a150d437c1c13b3dcc1
Boudewijn Danser <helpdesk@boudewijndanser.nl>
2018-05-17 14:57:38 +0200

    Making it shiny

--- client/src/components/signup/SignupPage.jsx
+++ client/src/components/signup/SignupPage.jsx
@@ -15,7 +15,7 @@ class SignupPage extends PureComponent {
 		)
 
 		return (
-			<div className="loginPage">
+			<div className="generalPage">
 				<h1>Sign up</h1>
 
 				<SignupForm onSubmit={this.handleSubmit} />
