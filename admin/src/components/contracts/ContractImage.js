fa100c560229ddd409d1bd6d1f74464135ebff0e
neenu <neenukurian92@gmail.com>
2018-05-22 20:45:35 +0200

    added css to contractimage

--- admin/src/components/contracts/ContractImage.js
+++ admin/src/components/contracts/ContractImage.js
@@ -3,25 +3,19 @@ import {connect} from 'react-redux'
 import {withStyles} from 'material-ui/styles';
 import Paper from 'material-ui/Paper';
 import {getContractImage} from '../../actions/contracts'
+import '../../css/ContractImage.css'
 
-export const styles = theme => ({
-    root: {
-      width: '100%',
-      marginTop: theme.spacing.unit * 3,
-      overflowX: 'auto',
-      display: 'flex'
-    }
-})
 class ContractImage extends PureComponent {
     componentWillMount() {
         this.props.getContractImage(this.props.match.params.id, this.props.match.params.image)
     }
     render(){
-        const {classes, contractImage} = this.props
+
+        
         return(
-            <Paper className={classes.root}>
-                <img alt='contractImage'
-                src={contractImage}
+            <Paper className="image-paper">
+                <img className='image-contract' alt='contractImage'
+                src={'http://jjlawfl.com/wp-content/uploads/2018/02/contract-breach-3-700x400.jpg'}
                 style={{maxHeight: '550px'}}/>
             </Paper>
         )
@@ -32,4 +26,4 @@ const mapStateToProps = (state) => ({
     contractImage: state.contractImage
 })
 
-export default withStyles(styles) (connect(mapStateToProps,{getContractImage})(ContractImage))
+export default (connect(mapStateToProps,{getContractImage})(ContractImage))
