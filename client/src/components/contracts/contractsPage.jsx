22a3dfc9d1e392fcaed0d5e8e11bfbe4cb727d43
Emacdyz <emeline.capdupuy@gmail.com>
2018-05-22 12:28:28 +0200

    fetching contracts

--- client/src/components/contracts/contractsPage.jsx
+++ client/src/components/contracts/contractsPage.jsx
@@ -2,34 +2,47 @@ import React, {PureComponent} from 'react'
 // import { Link } from 'react-router-dom'
 import BottomNav from '../layout/BottomNav'
 import {connect} from 'react-redux'
+import {getAllContracts} from '../../actions/contracts'
 
 //styling
+import Card, {CardContent} from 'material-ui/Card'
 import '../../css/index.css'
 import '../../css/contracts.css'
 
 
   class ContractsPage extends PureComponent {
 
-  render() {
-  
-    return (
-          <div>
-          <div className="generalPage">
-          <h1>Contracten</h1>
-          <p>
-            Vind u straks hier.
-           </p>
-          </div>
-          <div>
-          <BottomNav/>
-          </div>
-          </div>
-    )
-  }
+    componentWillMount() {
+      this.props.getAllContracts()
+    }
+
+    renderContract() {
+      const {contracts} = this.props
+      return (
+        <Card> 
+          
+        </Card>
+      )
+    }
+
+    render() {
+    
+      return (
+      <div>
+        <div className="overview">
+          <h1>Mijn contracten</h1>
+        </div>
+        
+
+      <BottomNav/>
+      </div>
+      )
+    }
 }
 
 const mapStateToProps = (state) => ({
-	currentUser: state.currentUser
+  currentUser: state.currentUser,
+  contracts: state.contracts
 })
 
-export default connect(mapStateToProps)(ContractsPage)
+export default connect(mapStateToProps, {getAllContracts})(ContractsPage)
