0c5e9f8f8923b16da8f4d4f9a6ece49ddf3ddced
John <john_ekelmans@msn.com>
2018-05-23 11:34:35 +0200

    delete contracts

--- client/src/reducers/contracts.js
+++ client/src/reducers/contracts.js
@@ -1,14 +1,15 @@
 import {GET_ALL_CONTRACTS, CONTRACT_DELETED} from '../actions/contracts'
 
 export default (state = [], {type, payload}) => {
-  
+
   switch (type) {
 
       case  GET_ALL_CONTRACTS:
       return payload
 
       case CONTRACT_DELETED:
-      return state.filter(contract => contract.id !== payload)
+      console.log('payload',payload);
+      return state = state.filter(contract =>{return contract.id !== parseInt(payload)})
 
     default:
       return state
