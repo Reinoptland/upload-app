2873089fa0ffbc85c2526fb7919cee85156d459b
atharv7 <virtuarchitects7@gmail.com>
2018-05-24 14:11:12 +0200

    deployable release

--- server/src/db.ts
+++ server/src/db.ts
@@ -29,12 +29,13 @@ class CustomNamingStrategy extends DefaultNamingStrategy implements NamingStrate
 export default () =>
   createConnection({
     type: "postgres",
-    url: process.env.DATABASE_URL || 'postgres://postgres:secret@localhost:5432/postgres',
+    url: process.env.DATABASE_URL || 'postgres://cwrygwiidlgzpg:801f7e5a8fc9a81d4dd23cc88cb842a022e47589af86345aefed329135ef3123@ec2-54-83-59-120.compute-1.amazonaws.com:5432/dbjtghd8khqouq',
     entities: [
       User,
       Contract,
       Admin
     ],
+    ssl: true,
     synchronize: true, // careful with this in production!
     logging: true,
     namingStrategy: new CustomNamingStrategy()
